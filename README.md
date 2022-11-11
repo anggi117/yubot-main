4ef30d404eef979b1464db653cca85116bccea95
Anggie Dwi Putra <mediatek567@gmail.com>
2022-11-11 14:38:14 +0700

    Edit readme and add new command card

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-###
-MASTER DUEL DISCORD BOT by anggi117
+## 👨‍💻 About This Bot :
+
+Yubot is Master Duel Discord for search any information of card.
+Fetching data from [Master Duel Meta](https://masterduelmeta.com) and [YGO Pro](https://ygopro.org) API
+
+## 🧰 Commands :
+
+- /card
+  Command for search information of card
+
+## Changelog :
+
+### 1.0
+
+Added command /card for get info of card
--- src/commands/tools/card.js
+++ src/commands/tools/card.js
@@ -30,6 +30,58 @@ const raritys = function (rarity) {
     }
   }
 };
+
+const card = function (data) {
+  if (data.type == "Monster") {
+    const typeRace = `[${data.monsterType[0]} / ${data.race} / ${data.type}]`;
+    const createData = {
+      typeRace: typeRace,
+    };
+    switch (data.monsterType[0]) {
+      case "Link": {
+        createData.description = data.description;
+      }
+      case "Pendulum": {
+        const pendulumEffect = data.description
+          .split("[ Pendulum Effect ] ")[1]
+          .split("[ Monster Effect ]")[0];
+
+        const monsterEffect = data.description
+          .split("[ Monster Effect ] ")[1]
+          .split("[ Pendulum Effect ]")[0];
+
+        const description = `**Pendulum Effect**\n${pendulumEffect}**Monster Effect**\n${monsterEffect}`;
+        createData.description = description;
+        break;
+      }
+
+      default: {
+        createData.description = data.description;
+        break;
+      }
+    }
+    return createData;
+  }
+  const typeRace = `[${data.race} / ${data.type}]`;
+  const createData = {
+    typeRace: typeRace,
+    description: data.description,
+  };
+  return createData;
+};
+
+const obtain = function (data) {
+  let forEmbed = "";
+  for (let i = 0; i < data.obtain.length; i++) {
+    const datas = {
+      name: data.obtain[i].source.name,
+      url: data.obtain[i].source.name.split(" ").join("-"),
+    };
+
+    forEmbed += `[${datas.name}](https://masterduelmeta.com/article/sets/${datas.url})\n`;
+  }
+  return forEmbed;
+};
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("card")
@@ -45,30 +97,31 @@ module.exports = {
     const data = await mdm(ygoProData.name);
 
     const raritysEmoji = raritys(data.rarity);
-    console.log("=====================Ini data MD=====================");
+    const cards = card(data);
     const convertName = encodeURIComponent(data.name);
+    const obtains = obtain(data);
+
     const embed = new EmbedBuilder()
       .setColor(0x0099ff)
       .setTitle(`${data.name} ${raritysEmoji}`)
       .setURL(`https://masterduelmeta.com/cards/${convertName}`)
-      .setDescription(`${data.description}`)
-      .setThumbnail(
-        `https://storage.googleapis.com/ygoprodeck.com/pics_artgame/${ygoProData.id}.jpg`
+      .setDescription(
+        `Attribute: ${data.attribute}\nLevel / Rank: ${data.level}\nATK / DEF : ${data.atk} / ${data.def}`
       )
-      .addFields(
-        {
-          name: "Regular field title",
-          value: `Card name : ${data.name}\nType : ${data.type}\nDescription : ${data.description}`,
-        },
-        { name: "\u200B", value: "\u200B" },
-        { name: "Attack", value: `${data.atk}`, inline: true },
-        { name: "Defense", value: `${data.def}`, inline: true }
-      )
-      .setTimestamp()
-      .setFooter({
-        text: "Some footer text here",
-        iconURL: `https://storage.googleapis.com/ygoprodeck.com/pics_artgame/${ygoProData.id}.jpg`,
-      });
+      .setThumbnail(`https://s3.lain.dev/ygo/${ygoProData.id}.webp`)
+      .addFields({
+        name: cards.typeRace,
+        value: cards.description,
+      })
+      .addFields({
+        name: "**How To Obtain**",
+        value: obtains,
+      })
+      .setTimestamp();
+    // .setFooter({
+    //   text: "Some footer text here",
+    //   iconURL: `https://storage.googleapis.com/ygoprodeck.com/pics_artgame/${ygoProData.id}.jpg`,
+    // });
     await interaction.reply({
       embeds: [embed],
     });
